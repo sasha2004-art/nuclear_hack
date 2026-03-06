890bbbe0cc582dd99532ecab89280b9bb7ef0595
maksi <maksim.v1liev@gmail.com>
2026-03-06 22:01:36 +0300

    ios fix

--- plotix_mobile/composeApp/src/iosMain/kotlin/com/example/plotix_mobile/MainViewController.kt
+++ plotix_mobile/composeApp/src/iosMain/kotlin/com/example/plotix_mobile/MainViewController.kt
@@ -1,6 +1,5 @@
 package com.example.plotix_mobile
 
 import androidx.compose.ui.window.ComposeUIViewController
-import com.example.plotix_mobile.ui.screens.MainScreen
 
 fun MainViewController() = ComposeUIViewController { App() }
